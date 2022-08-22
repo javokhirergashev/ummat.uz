1736403784160ad2513f73e73aea699af245242f
javokhirergashev <iqtidorziyosi@gmail.com>
2022-08-22 13:33:05 +0500

    uploaded frontend widgets

---
+++ web/frontend-files/js/template.js
@@ -0,0 +1,19 @@
+//[Master Javascript]
+
+//Project:	Travel club - Onepage Html Responsive Template
+//Version:	1.1
+//Last change:	28/04/2017 [fixed bug]
+//Primary use:	Travel club - Onepage Html Responsive Template 
+
+
+//Template script here
+
+$(document).ready(function(){
+    "use strict"; // Start of use strict
+
+	// prettyPhoto
+
+	$("a[rel^='alternate']").prettyPhoto();
+	
+   
+}); // End of use strict
