ac9057393a47df0cc8c519997aac57a921845916
Umidbek01 <umidjon331101@gmail.com>
2022-08-22 17:35:17 +0500

    footer

--- widgets/Footer.php
+++ widgets/Footer.php
@@ -2,12 +2,14 @@
 
 namespace app\widgets;
 
+use app\models\Contacts;
 use yii\bootstrap4\Widget;
 
 class Footer extends Widget
 {
     public function run()
     {
-        return $this->render('footer');
+        $contacts = Contacts::find()->all();
+        return $this->render('footer', compact("contacts"));
     }
 }
--- widgets/views/footer.php
+++ widgets/views/footer.php
@@ -1,7 +1,11 @@
 <!--Footer Section-->
 <footer class="footer" id="footer"><!-- Section id -->
+
     <div class="container">
+
         <div class="row">
+            <?php if(!empty($contacts)):?>
+            <?php foreach ($contacts as $contact):?>
             <div class="col-lg-12 text-center footer-logo wow fadeInDown"><!-- logo -->
                 <a href="#wrapper"><img src="/frontend-files/images/logo-white.png" class="img-responsive" alt=""></a>
             </div>
@@ -9,21 +13,21 @@
                 <div class="footer-block"><!-- footer block -->
                     <img src="/frontend-files/images/call-icon.png" alt="Call"><!-- call icon -->
                     <h5>Toll Number</h5>
-                    <h4></h4>
+                    <h4><?=$contact->first_phone?></h4>
                 </div>
             </div>
             <div class="col-md-4 col-sm-4 wow fadeInDown">
                 <div class="footer-block"><!-- footer block -->
                     <img src="/frontend-files/images/mail-icon.png" alt="Email"><!-- mail icon -->
                     <h5>Info Services</h5>
-                    <h4>info@Travelclub.com</h4>
+                    <h4><?=$contact->email?></h4>
                 </div>
             </div>
             <div class="col-md-4 col-sm-4 wow fadeInRight">
                 <div class="footer-block"><!-- footer block -->
                     <img src="/frontend-files/images/location-icon.png" alt="Location"><!-- location icon -->
                     <h5>Address</h5>
-                    <h4>A. 12 Lorem di Sheshi, CA 010102, US</h4>
+                    <h4><?=$contact->addres?></h4>
                 </div>
             </div>
             <div class="col-md-12 wow fadeInUp">
@@ -36,7 +40,10 @@
                     </ul>
                 </div>
             </div>
+                <?php endforeach;?>
+            <?php endif;?>
         </div>
+
     </div>
     <div class="container-fluid">
         <div class="row">
@@ -45,4 +52,5 @@
             </div>
         </div>
     </div>
+
 </footer>
