10eb5264a1518d7997e72fd12d05e90500517a73
javokhirergashev <iqtidorziyosi@gmail.com>
2022-08-22 12:41:16 +0500

    uploaded frontend widgets

--- widgets/Navigation.php
+++ widgets/Navigation.php
@@ -1,12 +1,14 @@
 <?php
 namespace app\widgets;
 
+use app\models\Menu;
 use yii\bootstrap4\Widget;
 
 class Navigation extends Widget
 {
     public function run()
     {
-        return $this->render('navigation');
+        $models = Menu::find()->where(['status'=>1])->orderBy(["order_by"=>SORT_ASC])->all();
+        return $this->render('navigation',compact('models'));
     }
 }
